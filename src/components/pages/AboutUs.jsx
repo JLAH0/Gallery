9447469948a147d614a54551b6cb26a0b6d16d06
Jean Luc <jlah@oxo.mu>
2023-08-19 03:11:42 +0400

    add

--- src/components/pages/AboutUs.jsx
+++ src/components/pages/AboutUs.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import '../../App.css';
-import Footer from './components/Footer';
 
 export default function AboutUs() {
   return <h1 className='aboutus'>About Us</h1>;
